2f6e2e357148b83331529d802c9c970d02f4eb91
junydania <o.dania@aol.com>
2017-03-16 21:02:04 +0100

    Create class methods

--- classes.rb
+++ classes.rb
@@ -1,21 +1,32 @@
+#Trying Out Class Methods
+
 class Animal
+
     attr_accessor :name
     attr_writer :color
-    attr_reader :legs, :arms
+    attr_reader :legs, :arms, :origin
 
-    @@species = ['cat', "Dog", "Rabbit", 'Tiger']
+    @@species = ['cat','cow','rabbit', 'tortoise', 'Zebra']
 
     def self.all_species
         @@species
     end
 
-    def self.create_with_Attribute(noise,color)
-        animal = self.new(noise)
-        animal.color = color
-        return animal
+    def self.create_with_attribute(noise, color)
+       animal =  self.new(noise)
+       animal.color = color
+       return animal
     end
 
-    def noise=(noise) #Setter method
+    def initialize(noise, legs="2 Legs", arms="4 arms")
+        @noise = noise
+        @legs = legs
+        @arms = arms
+        puts "A new animal has been created from class Animal"
+    end
+
+
+    def noise=(noise) #setter metthod
         @noise = noise
     end
 
@@ -27,21 +38,29 @@ class Animal
         "#{@name} is a #{@color} dog."
     end
 
-    def initialize
-        @legs = "Got 4 legs"
-        @arms = "Got 2 arms"
-        puts "A new animal has been instantiated."
+    def origin=(origin)
+        @origin = origin
     end
 
 end
 
-animal1 = Animal.new
+puts Animal.all_species.inspect
+
+animal1 = Animal.new("Got 4 legs", "Got no arm")
 
 animal1.noise=("cry!")
 puts animal1.noise
 
-animal1.name=("Snoop")
+animal1.name = "Steve"
 puts animal1.name
 
-animal1.color=("Dark Tan")
-puts animal1.color
+puts animal1.legs
+puts animal1.arms
+
+animal1.origin = "Sweden"
+puts animal1.origin
+
+animal3 = Animal.create_with_attribute("Quack!","Purple")
+animal3.name = "Mufasa"
+puts animal3.noise
+puts animal3.color
